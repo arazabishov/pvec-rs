800cadb7789a75f1d3ec76af62f0ed8bdff00855
Araz Abishov <araz@abishov.com>
2018-02-03 14:04:39 +0100

    Added a simple test for checking behaviour of shift function, which derives index of subtrie given a key and shift.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
+const MASK: usize = 0b11111;
+
+fn shift(key: usize, shift: usize) -> usize {
+    return (key >> shift) & MASK;
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn shift_must_return_correct_index() {
+        // 0d141 - 0b10001101
+        assert_eq!(shift(141, 0), 0b01101);
+        assert_eq!(shift(141, 5), 0b00100);
+        assert_eq!(shift(141, 10), 0b0000);
+        assert_eq!(shift(141, 15), 0b0000);
+        assert_eq!(shift(141, 20), 0b0000);
+        assert_eq!(shift(141, 25), 0b0000);
+        assert_eq!(shift(141, 30), 0b0000);
+        assert_eq!(shift(141, 25), 0b0000);
     }
 }
