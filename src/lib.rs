4f2010ca0c76e08e8cb3a017d82492a8934505e7
Araz Abishov <araz@abishov.com>
2018-02-19 20:11:19 +0100

    Slightly updated the test.

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,11 @@ mod tests {
         // 0d141 - 0b10001101
         assert_eq!(shift(141, 0), 0b01101);
         assert_eq!(shift(141, 5), 0b00100);
-        assert_eq!(shift(141, 10), 0b0000);
-        assert_eq!(shift(141, 15), 0b0000);
-        assert_eq!(shift(141, 20), 0b0000);
-        assert_eq!(shift(141, 25), 0b0000);
-        assert_eq!(shift(141, 30), 0b0000);
-        assert_eq!(shift(141, 25), 0b0000);
+        assert_eq!(shift(141, 10), 0b00000);
+        assert_eq!(shift(141, 15), 0b00000);
+        assert_eq!(shift(141, 20), 0b00000);
+        assert_eq!(shift(141, 25), 0b00000);
+        assert_eq!(shift(141, 30), 0b00000);
+        assert_eq!(shift(141, 25), 0b00000);
     }
 }
