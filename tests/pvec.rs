464ee7228527955be7f6899f4e8ca4dee32df68e
Araz Abishov <araz@abishov.com>
2018-03-06 19:21:04 +0100

    Imports more tests from dogged

--- tests/pvec.rs
+++ tests/pvec.rs
@@ -3,16 +3,49 @@ extern crate persistent;
 use persistent::pvec::PVec;
 
 #[test]
-fn new_must_return_correctly_initialized_pvec_instance() {
-    let mut vec = PVec::new();
+fn len_matches_actual_size() {
+    const N: usize = 5000;
 
-    for i in 0..64 {
-        vec.push(i);
+    let mut pvec = PVec::new();
+
+    for i in 0..N {
+        pvec.push(i);
     }
 
-    for i in 0..64 {
-        assert_eq!(vec[i], i);
+    assert_eq!(pvec.len(), N);
+
+    for i in 0..N {
+        assert_eq!(*pvec.get(i).unwrap(), i);
+    }
+}
+
+#[test]
+fn len_matches_len_cloned() {
+    const N: usize = 5000;
+
+    let mut pvec = PVec::new();
+
+    for i in 0..N {
+        pvec.push(i);
     }
 
-    assert_eq!(vec.len(), 64);
+    let pvec_0 = pvec.clone();
+    assert_eq!(pvec.len(), N);
+    assert_eq!(pvec_0.len(), N);
+
+    for i in 0..N {
+        pvec.push(i);
+    }
+
+    assert_eq!(pvec.len(), 2 * N);
+    assert_eq!(pvec_0.len(), N);
+
+    for i in 0..N {
+        assert_eq!(*pvec.get(i).unwrap(), i);
+        assert_eq!(*pvec_0.get(i).unwrap(), i);
+    }
+
+    for i in 0..N {
+        assert_eq!(*pvec.get(i + N).unwrap(), i);
+    }
 }
