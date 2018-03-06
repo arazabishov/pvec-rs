577efa916392d6ddf77f28822c8ad7923ea92fa6
Araz Abishov <araz@abishov.com>
2018-03-06 19:43:01 +0100

    Added a test to make sure that mutating pvec does not affect its clone.

--- tests/pvec.rs
+++ tests/pvec.rs
@@ -49,3 +49,37 @@ fn len_matches_len_cloned() {
         assert_eq!(*pvec.get(i + N).unwrap(), i);
     }
 }
+
+#[test]
+fn mutate_in_place_must_not_mutate_cloned_pvec() {
+    const N: usize = 32 * 4;
+
+    let mut pvec = PVec::new();
+
+    for i in 0..N {
+        pvec.push(i);
+    }
+
+    let pvec_0 = pvec.clone();
+    assert_eq!(pvec.len(), N);
+    assert_eq!(pvec_0.len(), N);
+
+    for i in 0..(N / 2) {
+        *pvec.get_mut(i).unwrap() += 1;
+    }
+
+    assert_eq!(pvec.len(), N);
+    assert_eq!(pvec_0.len(), N);
+
+    for i in 0..(N / 2) {
+        assert_eq!(*pvec.get(i).unwrap(), i + 1);
+        assert_eq!(*pvec_0.get(i).unwrap(), i);
+    }
+
+    // the second half ought to be untouched
+    for i in N / 2..N {
+        assert_eq!(*pvec.get(i).unwrap(), i);
+        assert_eq!(*pvec_0.get(i).unwrap(), i);
+        assert_eq!(pvec.get(i).unwrap() as *const usize, pvec_0.get(i).unwrap() as *const usize);
+    }
+}
