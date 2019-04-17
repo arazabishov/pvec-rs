ced432e881f70a9a411e212e026097d8529588a5
Araz Abishov <araz@abishov.com>
2019-04-17 20:39:20 +0200

    Experimental API for iterators

--- src/pvec/mod.rs
+++ src/pvec/mod.rs
@@ -182,6 +182,37 @@ impl<T: Clone + Debug> ops::IndexMut<usize> for PVec<T> {
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct PVecIter<T> {
+    // - you should avoid heap allocation in iterators
+    pvec: PVec<T>,
+    len: usize,
+}
+
+impl<T: Clone + Debug> Iterator for PVecIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        return None;
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len, Some(self.len))
+    }
+}
+
+impl<T: Clone + Debug> IntoIterator for PVec<T> {
+    type Item = T;
+    type IntoIter = PVecIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        PVecIter {
+            len: self.len(),
+            pvec: self, 
+        }
+    }
+}
+
 mod serializer {
     extern crate serde;
     extern crate serde_json;
