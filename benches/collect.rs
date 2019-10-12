af912cdd4b6a2e1757b0a03d3cee5cb734d46360
Araz Abishov <araz@abishov.com>
2019-10-12 16:12:10 +0200

    Tweaks with_fold benches

--- benches/collect.rs
+++ benches/collect.rs
@@ -4,17 +4,23 @@ use rayon::prelude::*;
 mod util_stdvec {
     use rayon::prelude::*;
 
-    /// Do whatever `collect` does by default.
-    pub fn collect<T, PI>(pi: PI) -> Vec<T>
+    // TODO: sequential iter does not have fold or reduce
+    // TODO: also, it doesn't make much sense to split and combine
+    //   stuff without changing it in any way, fuck.
+
+    /// Fold into vectors and then reduce them together.
+    pub fn fold_seq<T, I>(i: I) -> Vec<T>
     where
-        T: Send,
-        PI: ParallelIterator<Item = T> + Send,
+        I: Iterator<Item = T>,
     {
-        pi.collect()
+        i.fold(Vec::new(), |mut vec, x| {
+            vec.push(x);
+            vec
+        })
     }
 
     /// Fold into vectors and then reduce them together.
-    pub fn fold<T, PI>(pi: PI) -> Vec<T>
+    pub fn fold_par<T, PI>(pi: PI) -> Vec<T>
     where
         T: Send,
         PI: ParallelIterator<Item = T> + Send,
@@ -35,17 +41,19 @@ mod util_rrbvec {
     use rayon::prelude::*;
     use std::fmt::Debug;
 
-    /// Do whatever `collect` does by default.
-    pub fn collect<T, PI>(pi: PI) -> RrbVec<T>
+    pub fn fold_seq<T, I>(i: I) -> RrbVec<T>
     where
-        T: Send + Sync + Clone + Debug,
-        PI: ParallelIterator<Item = T> + Send,
+        T: Clone + Debug,
+        I: Iterator<Item = T>,
     {
-        pi.collect()
+        i.fold(RrbVec::new(), |mut vec, x| {
+            vec.push(x);
+            vec
+        })
     }
 
     /// Fold into vectors and then reduce them together.
-    pub fn fold<T, PI>(pi: PI) -> RrbVec<T>
+    pub fn fold_par<T, PI>(pi: PI) -> RrbVec<T>
     where
         T: Send + Sync + Clone + Debug,
         PI: ParallelIterator<Item = T> + Send,
@@ -66,17 +74,20 @@ mod util_pvec {
     use rayon::prelude::*;
     use std::fmt::Debug;
 
-    /// Do whatever `collect` does by default.
-    pub fn collect<T, PI>(pi: PI) -> PVec<T>
+    /// Fold into vectors and then reduce them together.
+    pub fn fold_seq<T, I>(i: I) -> PVec<T>
     where
-        T: Send + Sync + Clone + Debug,
-        PI: ParallelIterator<Item = T> + Send,
+        T: Clone + Debug,
+        I: Iterator<Item = T>,
     {
-        pi.collect()
+        i.fold(PVec::new(), |mut vec, x| {
+            vec.push(x);
+            vec
+        })
     }
 
     /// Fold into vectors and then reduce them together.
-    pub fn fold<T, PI>(pi: PI) -> PVec<T>
+    pub fn fold_par<T, PI>(pi: PI) -> PVec<T>
     where
         T: Send + Sync + Clone + Debug,
         PI: ParallelIterator<Item = T> + Send,
@@ -94,24 +105,25 @@ mod util_pvec {
 
 mod stdvec {
     use rayon::prelude::*;
+
+    pub fn generate_iter(n: u32) -> impl Iterator<Item = u32> {
+        let mut vec = Vec::new();
+        for i in 0_u32..n {
+            vec.push(i);
+        }
+        vec.into_iter().map(|x| x + 1)
+    }
+
     pub fn generate_indexed_iter(n: u32) -> impl IndexedParallelIterator<Item = u32> {
         let mut vec = Vec::new();
         for i in 0_u32..n {
             vec.push(i);
         }
-        vec.into_par_iter()
+        vec.into_par_iter().map(|x| x + 1)
     }
 
     pub fn generate_filter(n: u32) -> impl ParallelIterator<Item = u32> {
-        let mut vec = Vec::new();
-        for i in 0_u32..n {
-            vec.push(i);
-        }
-        vec.into_par_iter().filter(|_| true)
-    }
-
-    pub fn check(v: &[u32], n: u32) {
-        assert!(v.iter().cloned().eq(0..n));
+        generate_indexed_iter(n).filter(|_| true)
     }
 }
 
@@ -119,26 +131,24 @@ mod rrbvec {
     use pvec::core::RrbVec;
     use rayon::prelude::*;
 
+    pub fn generate_iter(n: u32) -> impl Iterator<Item = u32> {
+        let mut pvec = RrbVec::new();
+        for i in 0_u32..n {
+            pvec.push(i);
+        }
+        pvec.into_iter().map(|x| x + 1)
+    }
+
     pub fn generate_indexed_iter(n: u32) -> impl IndexedParallelIterator<Item = u32> {
         let mut pvec = RrbVec::new();
         for i in 0_u32..n {
             pvec.push(i);
         }
-        pvec.into_par_iter()
+        pvec.into_par_iter().map(|x| x + 1)
     }
 
     pub fn generate_filter(n: u32) -> impl ParallelIterator<Item = u32> {
-        let mut pvec = RrbVec::new();
-        for i in 0_u32..n {
-            pvec.push(i);
-        }
-        pvec.into_par_iter().filter(|_| true)
-    }
-
-    pub fn check(v: &RrbVec<u32>, n: u32) {
-        for i in 0..n {
-            assert_eq!(*v.get(i as usize).unwrap(), i);
-        }
+        generate_indexed_iter(n).filter(|_| true)
     }
 }
 
@@ -146,78 +156,60 @@ mod pvec {
     use pvec::PVec;
     use rayon::prelude::*;
 
+    pub fn generate_iter(n: u32) -> impl Iterator<Item = u32> {
+        let mut pvec = PVec::new();
+        for i in 0_u32..n {
+            pvec.push(i);
+        }
+        pvec.into_iter().map(|x| x + 1)
+    }
+
     pub fn generate_indexed_iter(n: u32) -> impl IndexedParallelIterator<Item = u32> {
         let mut pvec = PVec::new();
         for i in 0_u32..n {
             pvec.push(i);
         }
-        pvec.into_par_iter()
+        pvec.into_par_iter().map(|x| x + 1)
     }
 
     pub fn generate_filter(n: u32) -> impl ParallelIterator<Item = u32> {
-        let mut pvec = PVec::new();
-        for i in 0_u32..n {
-            pvec.push(i);
-        }
-        pvec.into_par_iter().filter(|_| true)
-    }
-
-    pub fn check(v: &PVec<u32>, n: u32) {
-        for i in 0..n {
-            assert_eq!(*v.get(i as usize).unwrap(), i);
-        }
+        generate_indexed_iter(n).filter(|_| true)
     }
 }
 
+// TODO: add benchmark which supports filtered iterator
+// TODO: add benchmark which measures split_time as well
+// TODO: add benchmark which evaluates naive append as well
+// TODO: stop using modules here, because they introduce additional overhead
+
 macro_rules! make_bench {
     ($generate:ident, $postfix:literal) => {
-        fn with_collect(criterion: &mut Criterion, num_threads: usize) {
-            let mut group = criterion.benchmark_group(format!(
-                "with_collect_{}_with_thread_num_{}",
-                $postfix, num_threads
-            ));
-
+        fn with_fold_seq(criterion: &mut Criterion) {
+            let mut group = criterion.benchmark_group(format!("with_fold_seq_{}", $postfix));
             group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
 
             let params = vec![
                 10, 20, 40, 60, 80, 100, 200, 400, 600, 800, 1000, 2000, 4000, 6000, 8000, 10000,
                 20000, 40000,
             ];
+
             for p in params.iter() {
                 group.bench_with_input(BenchmarkId::new(STD_VEC, p), p, |b, n| {
-                    let pool = rayon::ThreadPoolBuilder::new()
-                        .num_threads(num_threads)
-                        .build()
-                        .unwrap();
-
-                    pool.install(|| {
-                        let mut vec = None;
-                        b.iter(|| vec = Some(util_stdvec::collect(stdvec::$generate(*n))));
-                        stdvec::check(&vec.unwrap(), *n);
+                    b.iter(|| {
+                        let data = stdvec::generate_iter(*n);
+                        util_stdvec::fold_seq(data)
                     });
                 });
                 group.bench_with_input(BenchmarkId::new(RRBVEC_UNBALANCED, p), p, |b, n| {
-                    let pool = rayon::ThreadPoolBuilder::new()
-                        .num_threads(num_threads)
-                        .build()
-                        .unwrap();
-
-                    pool.install(|| {
-                        let mut vec = None;
-                        b.iter(|| vec = Some(util_rrbvec::collect(rrbvec::$generate(*n))));
-                        rrbvec::check(&vec.unwrap(), *n);
+                    b.iter(|| {
+                        let data = rrbvec::generate_iter(*n);
+                        util_rrbvec::fold_seq(data)
                     });
                 });
                 group.bench_with_input(BenchmarkId::new(PVEC_UNBALANCED, p), p, |b, n| {
-                    let pool = rayon::ThreadPoolBuilder::new()
-                        .num_threads(num_threads)
-                        .build()
-                        .unwrap();
-
-                    pool.install(|| {
-                        let mut vec = None;
-                        b.iter(|| vec = Some(util_pvec::collect(pvec::$generate(*n))));
-                        pvec::check(&vec.unwrap(), *n);
+                    b.iter(|| {
+                        let data = pvec::generate_iter(*n);
+                        util_pvec::fold_seq(data)
                     });
                 });
             }
@@ -225,7 +217,7 @@ macro_rules! make_bench {
             group.finish();
         }
 
-        fn with_fold(criterion: &mut Criterion, num_threads: usize) {
+        fn with_fold_par(criterion: &mut Criterion, num_threads: usize) {
             let mut group = criterion.benchmark_group(format!(
                 "with_fold_{}_with_thread_num_{}",
                 $postfix, num_threads
@@ -244,13 +236,10 @@ macro_rules! make_bench {
                         .unwrap();
 
                     pool.install(|| {
-                        let mut vec = None;
-                        b.iter_batched(
-                            || stdvec::generate_indexed_iter(*n).with_min_len(2048),
-                            |data| vec = Some(util_stdvec::fold(data)),
-                            BatchSize::SmallInput,
-                        );
-                        stdvec::check(&vec.unwrap(), *n);
+                        b.iter(|| {
+                            let data = stdvec::generate_indexed_iter(*n).with_max_len(2048);
+                            util_stdvec::fold_par(data)
+                        });
                     });
                 });
                 group.bench_with_input(BenchmarkId::new(RRBVEC_UNBALANCED, p), p, |b, n| {
@@ -260,13 +249,10 @@ macro_rules! make_bench {
                         .unwrap();
 
                     pool.install(|| {
-                        let mut vec = None;
-                        b.iter_batched(
-                            || rrbvec::generate_indexed_iter(*n).with_min_len(2048),
-                            |data| vec = Some(util_rrbvec::fold(data)),
-                            BatchSize::SmallInput,
-                        );
-                        rrbvec::check(&vec.unwrap(), *n);
+                        b.iter(|| {
+                            let data = rrbvec::generate_indexed_iter(*n).with_max_len(2048);
+                            util_rrbvec::fold_par(data)
+                        });
                     });
                 });
                 group.bench_with_input(BenchmarkId::new(PVEC_UNBALANCED, p), p, |b, n| {
@@ -276,13 +262,10 @@ macro_rules! make_bench {
                         .unwrap();
 
                     pool.install(|| {
-                        let mut vec = None;
-                        b.iter_batched(
-                            || pvec::generate_indexed_iter(*n).with_min_len(2048),
-                            |data| vec = Some(util_pvec::fold(data)),
-                            BatchSize::SmallInput,
-                        );
-                        pvec::check(&vec.unwrap(), *n);
+                        b.iter(|| {
+                            let data = pvec::generate_indexed_iter(*n).with_max_len(2048);
+                            util_pvec::fold_par(data);
+                        });
                     });
                 });
             }
@@ -290,60 +273,24 @@ macro_rules! make_bench {
             group.finish();
         }
 
-        pub fn with_collect_1(criterion: &mut Criterion) {
-            with_collect(criterion, 1);
-        }
-
-        pub fn with_collect_2(criterion: &mut Criterion) {
-            with_collect(criterion, 2);
-        }
-
-        pub fn with_collect_4(criterion: &mut Criterion) {
-            with_collect(criterion, 4);
-        }
-
-        pub fn with_collect_8(criterion: &mut Criterion) {
-            with_collect(criterion, 8);
-        }
-
-        pub fn with_collect_16(criterion: &mut Criterion) {
-            with_collect(criterion, 16);
-        }
-
-        pub fn with_collect_32(criterion: &mut Criterion) {
-            with_collect(criterion, 32);
-        }
-
-        pub fn with_collect_64(criterion: &mut Criterion) {
-            with_collect(criterion, 64);
-        }
-
         pub fn with_fold_1(criterion: &mut Criterion) {
-            with_fold(criterion, 1);
+            with_fold_seq(criterion);
         }
 
         pub fn with_fold_2(criterion: &mut Criterion) {
-            with_fold(criterion, 2);
+            with_fold_par(criterion, 2);
         }
 
         pub fn with_fold_4(criterion: &mut Criterion) {
-            with_fold(criterion, 4);
+            with_fold_par(criterion, 4);
         }
 
         pub fn with_fold_8(criterion: &mut Criterion) {
-            with_fold(criterion, 8);
+            with_fold_par(criterion, 8);
         }
 
         pub fn with_fold_16(criterion: &mut Criterion) {
-            with_fold(criterion, 16);
-        }
-
-        pub fn with_fold_32(criterion: &mut Criterion) {
-            with_fold(criterion, 32);
-        }
-
-        pub fn with_fold_64(criterion: &mut Criterion) {
-            with_fold(criterion, 64);
+            with_fold_par(criterion, 16);
         }
     };
 }
@@ -362,32 +309,14 @@ mod vec_i_filtered {
 
 criterion_group!(
     benches,
-    vec_i::with_collect_1,
-    vec_i::with_collect_2,
-    vec_i::with_collect_4,
-    vec_i::with_collect_8,
-    vec_i::with_collect_16,
-    vec_i::with_collect_32,
-    vec_i::with_collect_64,
     vec_i::with_fold_1,
     vec_i::with_fold_2,
     vec_i::with_fold_4,
     vec_i::with_fold_8,
     vec_i::with_fold_16,
-    vec_i::with_fold_32,
-    vec_i::with_fold_64,
-    vec_i_filtered::with_collect_1,
-    vec_i_filtered::with_collect_2,
-    vec_i_filtered::with_collect_4,
-    vec_i_filtered::with_collect_8,
-    vec_i_filtered::with_collect_16,
-    vec_i_filtered::with_collect_32,
-    vec_i_filtered::with_collect_64,
-    vec_i_filtered::with_fold_1,
-    vec_i_filtered::with_fold_2,
-    vec_i_filtered::with_fold_4,
-    vec_i_filtered::with_fold_8,
-    vec_i_filtered::with_fold_16,
-    vec_i_filtered::with_fold_32,
-    vec_i_filtered::with_fold_64,
+    // vec_i_filtered::with_fold_1,
+    // vec_i_filtered::with_fold_2,
+    // vec_i_filtered::with_fold_4,
+    // vec_i_filtered::with_fold_8,
+    // vec_i_filtered::with_fold_16,
 );
