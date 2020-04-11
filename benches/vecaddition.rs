f057f70df01dcb860b81bec9e09b07240532303f
Araz Abishov <araz@abishov.com>
2020-04-11 13:50:47 +0200

    Updating vecaddition benches

--- benches/vecaddition.rs
+++ benches/vecaddition.rs
@@ -7,10 +7,10 @@ use crate::pvec::PVec;
 
 use criterion::BatchSize::SmallInput;
 
-macro_rules! vec_balanced {
-    ($vec:ident) => {
+macro_rules! generate_vec {
+    ($new_vec:expr) => {
         |n: usize| {
-            let mut vec = $vec::new();
+            let mut vec = $new_vec();
 
             for i in 0..n {
                 vec.push(i);
@@ -21,24 +21,15 @@ macro_rules! vec_balanced {
     };
 }
 
-macro_rules! vec_balanced_cloned {
-    ($vec:ident) => {
-        |n: usize| {
-            let generate_vec = vec_balanced!($vec);
-            generate_vec(n).clone()
-        }
-    };
-}
-
 fn vector_addition_seq(criterion: &mut Criterion) {
     macro_rules! bench {
-        ($vec:ident) => {
+        ($new_vec:expr) => {
             |(vec_one, vec_two)| {
                 vec_one
                     .into_iter()
                     .zip(vec_two)
                     .map(|(e_1, e_2)| e_1 + e_2)
-                    .fold($vec::new(), |mut vec_1, x| {
+                    .fold($new_vec(), |mut vec_1, x| {
                         vec_1.push(x);
                         vec_1
                     })
@@ -50,13 +41,13 @@ fn vector_addition_seq(criterion: &mut Criterion) {
     group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
 
     macro_rules! make_bench {
-        ($name:ident, $p:ident, $vec:ident) => {
+        ($name:ident, $p:ident, $new_vec:expr) => {
             group.bench_with_input(BenchmarkId::new($name, $p), $p, |b, n| {
-                let generate_vec = vec_balanced!($vec);
+                let generate_vec = generate_vec!($new_vec);
 
                 b.iter_batched(
                     || (generate_vec(*n), generate_vec(*n)),
-                    bench!($vec),
+                    bench!($new_vec),
                     SmallInput,
                 );
             });
@@ -68,38 +59,32 @@ fn vector_addition_seq(criterion: &mut Criterion) {
     ];
 
     for p in params.iter() {
-        make_bench!(STD_VEC, p, Vec);
-        make_bench!(RBVEC, p, RbVec);
-        make_bench!(RRBVEC, p, RrbVec);
+        make_bench!(STD_VEC, p, || Vec::new());
+        make_bench!(RBVEC, p, || RbVec::new());
+        make_bench!(RRBVEC, p, || RrbVec::new());
+        make_bench!(PVEC_STD, p, || PVec::new());
 
-        make_bench!(PVEC_STD, p, PVec);
-        group.bench_with_input(BenchmarkId::new(PVEC_RRBVEC_RELAXED, p), p, |b, n| {
-            let generate_vec = vec_balanced_cloned!(PVec);
-
-            b.iter_batched(
-                || (generate_vec(*n), generate_vec(*n)),
-                bench!(PVec),
-                SmallInput,
-            );
-        });
+        // TODO: verify that RRBVEC is relaxed here.
+        make_bench!(PVEC_RRBVEC_RELAXED, p, || PVec::new_with_tree());
     }
 
     group.finish();
 }
 
+// TODO: consider running single thread tests here too!
 fn vector_addition_par(criterion: &mut Criterion, num_threads: usize) {
     macro_rules! bench {
-        ($vec:ident) => {
+        ($new_vec:expr) => {
             |(vec_one, vec_two)| {
                 vec_one
                     .into_par_iter()
                     .zip(vec_two)
                     .map(|(e_1, e_2)| e_1 + e_2)
-                    .fold($vec::new, |mut vec_1, x| {
+                    .fold($new_vec, |mut vec_1, x| {
                         vec_1.push(x);
                         vec_1
                     })
-                    .reduce($vec::new, |mut vec_1, mut vec_2| {
+                    .reduce($new_vec, |mut vec_1, mut vec_2| {
                         vec_1.append(&mut vec_2);
                         vec_1
                     })
@@ -117,14 +102,14 @@ fn vector_addition_par(criterion: &mut Criterion, num_threads: usize) {
         .unwrap();
 
     macro_rules! make_bench {
-        ($name:ident, $p:ident, $vec:ident) => {
+        ($name:ident, $p:ident, $new_vec:expr) => {
             group.bench_with_input(BenchmarkId::new($name, $p), $p, |b, n| {
-                let generate_vec = vec_balanced!($vec);
+                let generate_vec = generate_vec!($new_vec);
 
                 pool.install(|| {
                     b.iter_batched(
                         || (generate_vec(*n), generate_vec(*n)),
-                        bench!($vec),
+                        bench!($new_vec),
                         SmallInput,
                     );
                 });
@@ -134,25 +119,17 @@ fn vector_addition_par(criterion: &mut Criterion, num_threads: usize) {
 
     let params = vec![
         20, 40, 60, 80, 100, 200, 400, 600, 800, 1000, 2000, 4000, 6000, 8000, 10000, 20000, 40000,
+        60000,
     ];
 
     for p in params.iter() {
-        make_bench!(STD_VEC, p, Vec);
-        make_bench!(RBVEC, p, RbVec);
-        make_bench!(RRBVEC, p, RrbVec);
+        make_bench!(STD_VEC, p, || Vec::new());
+        make_bench!(RBVEC, p, || RbVec::new());
+        make_bench!(RRBVEC, p, || RrbVec::new());
+        make_bench!(PVEC_STD, p, || PVec::new());
 
-        make_bench!(PVEC_STD, p, PVec);
-        group.bench_with_input(BenchmarkId::new(PVEC_RRBVEC_RELAXED, p), p, |b, n| {
-            let generate_vec = vec_balanced_cloned!(PVec);
-
-            pool.install(|| {
-                b.iter_batched(
-                    || (generate_vec(*n), generate_vec(*n)),
-                    bench!(PVec),
-                    SmallInput,
-                );
-            });
-        });
+        // TODO: check that the tree is actually relaxed
+        make_bench!(PVEC_RRBVEC_RELAXED, p, || PVec::new_with_tree());
     }
 
     group.finish();
