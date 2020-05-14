fbb5880e15ae2038e037be8450f94815d365d73a
Araz Abishov <araz@abishov.com>
2020-05-14 22:36:07 +0200

    Moving iter tests to integration tests file

--- tests/tests.rs
+++ tests/tests.rs
@@ -434,3 +434,276 @@ mod test_pvec {
         }
     }
 }
+
+macro_rules! make_iter_tests {
+    ($vec:ident, $module:ident) => {
+        mod $module {
+            use super::$vec;
+            use super::BRANCH_FACTOR;
+
+            #[test]
+            fn empty_vec() {
+                let vec_one: $vec<usize> = $vec::new();
+                let vec_two: $vec<usize> = $vec::new();
+
+                let mut iter_one = vec_one.into_iter();
+                let mut iter_two = vec_two.into_iter();
+
+                assert_eq!(iter_one.size_hint(), (0, Some(0)));
+                assert_eq!(iter_one.next(), None);
+
+                assert_eq!(iter_two.size_hint(), (0, Some(0)));
+                assert_eq!(iter_two.next_back(), None);
+            }
+
+            #[test]
+            fn has_tail_only() {
+                let mut vec_one = $vec::new();
+                let mut vec_two = $vec::new();
+
+                for i in 0..BRANCH_FACTOR {
+                    vec_one.push(i);
+                    vec_two.push(i);
+                }
+
+                let mut iter_one = vec_one.into_iter();
+                for i in 0..BRANCH_FACTOR {
+                    assert_eq!(iter_one.next(), Some(i));
+                }
+
+                let mut iter_two = vec_two.into_iter();
+                for i in (0..BRANCH_FACTOR).rev() {
+                    assert_eq!(iter_two.next_back(), Some(i));
+                }
+            }
+
+            #[test]
+            fn underlying_tree_has_multiple_levels() {
+                let mut vec_one = $vec::new();
+                let mut vec_two = $vec::new();
+
+                let mut val = 0;
+                for _ in 0..(BRANCH_FACTOR * BRANCH_FACTOR * BRANCH_FACTOR) {
+                    vec_one.push(val);
+                    vec_two.push(val);
+                    val += 1;
+                }
+
+                for _ in 0..(BRANCH_FACTOR / 2) {
+                    vec_one.push(val);
+                    vec_two.push(val);
+                    val += 1;
+                }
+
+                let len_one = vec_one.len();
+                let mut iter_one = vec_one.into_iter();
+
+                for i in 0..len_one {
+                    assert_eq!(iter_one.next(), Some(i));
+                }
+
+                let len_two = vec_two.len();
+                let mut iter_two = vec_two.into_iter();
+
+                for i in 0..len_two {
+                    assert_eq!(iter_two.next(), Some(i));
+                }
+            }
+
+            #[test]
+            fn underlying_tree_is_relaxed() {
+                let vec_size = 33;
+
+                let mut vec = $vec::new();
+                let mut vec_item = 0;
+
+                for i in 0..128 {
+                    if i % 2 == 0 {
+                        let mut vec_temp = $vec::new();
+
+                        for _ in 0..vec_size {
+                            vec_temp.push(vec_item);
+                            vec_item += 1;
+                        }
+
+                        assert_eq!(vec_temp.len(), vec_size);
+
+                        vec.append(&mut vec_temp);
+
+                        assert_eq!(vec_temp.len(), 0);
+                    } else {
+                        for _ in 0..(vec_size + vec_size) {
+                            vec.push(vec_item);
+                            vec_item += 1;
+                        }
+                    }
+
+                    assert_eq!(vec.len(), vec_item);
+
+                    for i in 0..vec.len() {
+                        assert_eq!(*vec.get(i).unwrap(), i);
+                        assert_eq!(*vec.get_mut(i).unwrap(), i);
+                    }
+
+                    let mut vec_one_clone = vec.clone();
+                    for i in (0..vec_item).rev() {
+                        assert_eq!(vec_one_clone.pop().unwrap(), i);
+                    }
+
+                    assert_eq!(vec_one_clone.len(), 0);
+                    assert_eq!(vec.len(), vec_item);
+
+                    let len = vec.len();
+
+                    let mut iter_one = vec.clone().into_iter();
+                    let mut iter_two = vec.clone().into_iter();
+
+                    for i in 0..len {
+                        assert_eq!(iter_one.next(), Some(i));
+                    }
+
+                    for i in (0..len).rev() {
+                        assert_eq!(iter_two.next_back(), Some(i));
+                    }
+                }
+            }
+
+            #[test]
+            fn sequential_calls_to_next_and_next_back() {
+                let mut vec = $vec::new();
+
+                let mut val = 0;
+                for _ in 0..(BRANCH_FACTOR * BRANCH_FACTOR * BRANCH_FACTOR) {
+                    vec.push(val);
+                    val += 1;
+                }
+
+                for _ in 0..(BRANCH_FACTOR / 2) {
+                    vec.push(val);
+                    val += 1;
+                }
+
+                let len = vec.len();
+                let mut iter = vec.into_iter();
+
+                let mut next_i = 0;
+                let mut next_back_i = len - 1;
+
+                while next_i <= next_back_i {
+                    assert_eq!(Some(next_i), iter.next());
+                    assert_eq!(Some(next_back_i), iter.next_back());
+
+                    next_i += 1;
+                    next_back_i -= 1;
+                }
+
+                assert_eq!(iter.size_hint(), (len, Some(len)));
+                assert_eq!(iter.next(), None);
+                assert_eq!(iter.next_back(), None);
+            }
+        }
+    };
+}
+
+make_iter_tests!(RbVec, test_iter_rbvec);
+make_iter_tests!(RrbVec, test_iter_rrbvec);
+
+mod test_iter_pvec {
+    use super::PVec;
+    use super::BRANCH_FACTOR;
+
+    #[test]
+    fn empty_vec() {
+        let pvec: PVec<usize> = PVec::new();
+        let mut iter = pvec.into_iter();
+
+        let size = iter.size_hint();
+        let next = iter.next();
+
+        assert_eq!(next, None);
+        assert_eq!(size, (0, Some(0)));
+    }
+
+    #[test]
+    fn has_tail_only() {
+        let mut pvec = PVec::new();
+
+        for i in 0..BRANCH_FACTOR {
+            pvec.push(i);
+        }
+
+        for (i, val) in pvec.into_iter().enumerate() {
+            assert_eq!(i, val);
+        }
+    }
+
+    #[test]
+    fn underlying_tree_has_multiple_levels() {
+        let mut pvec = PVec::new();
+
+        let mut val = 0;
+        for _ in 0..(BRANCH_FACTOR * BRANCH_FACTOR * BRANCH_FACTOR) {
+            pvec.push(val);
+            val += 1;
+        }
+
+        for _ in 0..(BRANCH_FACTOR / 2) {
+            pvec.push(val);
+            val += 1;
+        }
+
+        for (i, val) in pvec.into_iter().enumerate() {
+            assert_eq!(i, val);
+        }
+    }
+
+    #[test]
+    fn underlying_tree_is_relaxed() {
+        let vec_size = 33;
+
+        let mut vec = PVec::new();
+        let mut vec_item = 0;
+
+        for i in 0..128 {
+            if i % 2 == 0 {
+                let mut vec_temp = PVec::new();
+
+                for _ in 0..vec_size {
+                    vec_temp.push(vec_item);
+                    vec_item += 1;
+                }
+
+                assert_eq!(vec_temp.len(), vec_size);
+
+                vec.append(&mut vec_temp);
+
+                assert_eq!(vec_temp.len(), 0);
+            } else {
+                for _ in 0..(vec_size + vec_size) {
+                    vec.push(vec_item);
+                    vec_item += 1;
+                }
+            }
+
+            assert_eq!(vec.len(), vec_item);
+
+            for i in 0..vec.len() {
+                assert_eq!(*vec.get(i).unwrap(), i);
+                assert_eq!(*vec.get_mut(i).unwrap(), i);
+            }
+
+            let mut vec_one_clone = vec.clone();
+            for i in (0..vec_item).rev() {
+                assert_eq!(vec_one_clone.pop().unwrap(), i);
+            }
+
+            assert_eq!(vec_one_clone.len(), 0);
+            assert_eq!(vec.len(), vec_item);
+
+            let vec_clone = vec.clone();
+            for (i, val) in vec_clone.into_iter().enumerate() {
+                assert_eq!(i, val);
+            }
+        }
+    }
+}
