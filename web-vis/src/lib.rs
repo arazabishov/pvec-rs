a8e790f96ab2a099f2cacd1f0b4af19bab3611c1
Araz Abishov <arabisho@microsoft.com>
2022-12-28 12:29:39 +0400

    (snapshot): collapsible tree

--- web-vis/src/lib.rs
+++ web-vis/src/lib.rs
@@ -2,16 +2,47 @@ extern crate pvec;
 extern crate serde_json;
 
 use pvec::core::RrbVec;
-
 use wasm_bindgen::prelude::*;
 
+// We need to keep state on the WASM side because we rely on the identity of underlying objects.
+// If we serialize and send values over to JS we will lose identity of objects, which defeats the whole point of the demo.
+static mut STATE: Vec<RrbVec<usize>> = Vec::new();
+
 #[wasm_bindgen]
-pub fn gen_vec(size: usize) -> String {
+pub fn add_vec(size: usize) {
     let mut vec = RrbVec::new();
 
     for i in 0..size {
         vec.push(i);
     }
 
-    return serde_json::to_string(&vec).unwrap();
+    unsafe { STATE.push(vec) }
+}
+
+#[wasm_bindgen]
+pub fn split_vec(vec_idx: usize, idx: usize) {
+    unsafe {
+        STATE[vec_idx] = STATE.get_mut(vec_idx).unwrap().split_off(idx);
+     }
+}
+
+#[wasm_bindgen]
+pub fn clear() {
+    unsafe { STATE.clear() }
+}
+
+#[wasm_bindgen]
+pub fn get() -> Vec<JsValue> {
+    let mut vec = Vec::new();
+
+    unsafe {
+        for rrbvec in &STATE {
+            // println!("{:?}", );
+            vec.push(JsValue::from_str(
+                serde_json::to_string(&rrbvec).unwrap().as_str(),
+            ));
+        }
+    }
+
+    vec
 }
--- web-vis/www/index.js
+++ web-vis/www/index.js
@@ -1,101 +1,231 @@
 import * as wasm from "web-vis";
 import * as d3 from "d3";
 
-var margin = { top: 100, right: 10, bottom: 240, left: 10 };
-var height = 1080 - margin.top - margin.bottom;
-var width = 1820 - margin.left - margin.right;
+const margin = { top: 32, right: 120, bottom: 10, left: 512 };
+const width = 1400 + 512 - margin.left - margin.right;
 
-function redrawTree(size) {
-    d3.select("svg").remove(); 
+const dy = width / 12;
+const dx = 12;
 
-    let vec = JSON.parse(wasm.gen_vec(size));
-    console.log(vec);
+const diagonal = d3
+  .linkVertical()
+  .x((d) => d.x)
+  .y((d) => d.y);
+const tree = d3.tree().nodeSize([dx, dy]);
 
-    var orientations = {
-        "bottom-to-top": {
-            size: [width, height],
-            x: function(d) { return d.x; },
-            y: function(d) { return d.y; }
-        }
-    };
+wasm.add_vec(427);
 
-    var svg = d3.select("body")
-        .selectAll("svg")
-        .data(d3.entries(orientations))
-        .enter().append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+let vec = JSON.parse(wasm.get()[0]);
+console.log(wasm.get()[0]);
 
-    svg.each(function(orientation) {
+const getDescendants = (node) => {
+  if (!node) {
+    return null;
+  }
 
-        var svg = d3.select(this);
-        var o = orientation.value;
+  if (node.relaxedBranch) {
+    return node.relaxedBranch;
+  } else if (node.branch) {
+    return node.branch;
+  } else if (node.leaf) {
+    return node.leaf;
+  } else {
+    return node;
+  }
+};
 
-        // Compute the layout.
-        var treemap = d3.tree().size(o.size);
-            
-        var nodes = d3.hierarchy(vec.tree.root, node => {        
-            if (node == null) {
-                return null;
-            }
+function drawCollapsibleTree() {
+  const root = d3.hierarchy(vec.tree.root, getDescendants);
 
-            // console.log(node);
-            if (node.branch != null) {            
-                return node.branch;
-            } else if (node.leaf != null) {
-                return node.leaf;
-            } else {
-                return node;
-            }
-        });
-        nodes = treemap(nodes);
+  root.x0 = dy / 2;
+  root.y0 = 0;
 
-        var links = nodes.descendants().slice(1);
+  let descendants = root.descendants();
+  
+  let next_node_to_expand = descendants ? descendants[0].data : null;
+  let next_node_to_expand_i = 0;
 
-        // Create the link lines.
-        svg.selectAll(".link")
-            .data(links)
-            .enter().append("path")
-            .attr("class", "link")
-            .attr("d", function(d) {
-            return "M" + d.x + "," + o.y(d)
-                + "C" + d.x + "," + (o.y(d) + o.y(d.parent)) / 2
-                + " " + d.parent.x + "," +  (o.y(d) + o.y(d.parent)) / 2
-                + " " + d.parent.x + "," + o.y(d.parent);
-            });
+  // descendants are sorted in topological order
+  descendants.forEach((d, i) => {
+    const { children, data, parent } = d;
 
-        // Create the node circles.
-        var node = svg.selectAll(".node")
-            .data(nodes.descendants())
-            .enter()
-            .append("g");
+    d.id = i;
+    d._children = children;
 
-        node.append("circle")
-            .attr("class", "node")
-            .attr("r", 4.5)
-            .attr("cx", o.x)
-            .attr("cy", o.y);
+    // keep only the right-most branches expanded to save space
+    if (next_node_to_expand === data) {      
+      // if we have not exhausted all children of the parent, keep increasing the index
+      if (parent && next_node_to_expand_i < parent.data.len) {
+        const children = getDescendants(parent.data);
+        next_node_to_expand = children[next_node_to_expand_i++];
+      } else {
+        const children = getDescendants(data);
 
-        node.append("text")
-            .text(function (d) {
-                console.log(d);
+        next_node_to_expand_i = 0;
+        next_node_to_expand = children ? children[next_node_to_expand_i++] : null;
+      }
+    } else {
+      d.children = null;
+    }
+  });
 
-                if (Number.isInteger(d.data)) {
-                    return d.data;
-                } 
-                            
-                return ""
-            })
-            .attr("x", o.x)
-            .attr("dx", 5)
-            .attr("y", o.y);
+  // TODO: the first two lines have to change. This function should
+  // create an SVG and then return it to the caller, so then
+  // the caller can place it on the page appropriately.
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("viewBox", [-margin.left, -margin.top, width, dx])
+    .style("font", "10px sans-serif")
+    .style("user-select", "none");
+
+  const gLink = svg
+    .append("g")
+    .attr("fill", "none")
+    .attr("stroke", "#555")
+    .attr("stroke-opacity", 0.4)
+    .attr("stroke-width", 1.5);
+
+  const gNode = svg
+    .append("g")
+    .attr("cursor", "pointer")
+    .attr("pointer-events", "all");
+
+  function update(source) {
+    const duration = d3.event && d3.event.altKey ? 2500 : 250;
+    const nodes = root.descendants().reverse();
+    const links = root.links();
+
+    // Compute the new tree layout.
+    tree(root);
+
+    let top = root;
+    let bottom = root;
+    root.eachBefore((node) => {
+      if (node.y < top.y) top = node;
+      if (node.y > bottom.y) bottom = node;
     });
+
+    // +32 is a hack
+    const height = bottom.y - top.y + margin.top + margin.bottom + 32;
+
+    const transition = svg
+      .transition()
+      .duration(duration)
+      .attr("viewBox", [-margin.left, -margin.top, width, height])
+      .tween(
+        "resize",
+        window.ResizeObserver ? null : () => () => svg.dispatch("toggle")
+      );
+
+    // Update the nodes…
+    const node = gNode.selectAll("g").data(nodes, (d) => d.id);
+
+    // Enter any new nodes at the parent's previous position.
+    const nodeEnter = node
+      .enter()
+      .append("g")
+      .attr("transform", (d) => `translate(${source.x0},${source.y0})`)
+      .attr("fill-opacity", 0)
+      .attr("stroke-opacity", 0)
+      .on("click", (event, d) => {
+        d.children = d.children ? null : d._children;
+        update(d);
+      });
+
+    nodeEnter
+      .append("circle")
+      .attr("r", 2.5)
+      .attr("fill", (d) => (d._children ? "#555" : "#999"))
+      .attr("stroke-width", 10);
+
+    nodeEnter
+      .append("text")
+      .attr("transform", (d) => `rotate(90)`)
+      .attr("dy", "0.31em")
+      .attr("x", (d) => (d._children ? -8 : 8))
+      .attr("text-anchor", (d) => (d._children ? "end" : "start"))
+      .text((d) => {
+        if (Number.isInteger(d.data)) {
+          return d.data;
+        }
+
+        return null;
+      })
+      .clone(true)
+      .lower()
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-width", 3)
+      .attr("stroke", "white");
+
+    // Transition nodes to their new position.
+    const nodeUpdate = node
+      .merge(nodeEnter)
+      .transition(transition)
+      .attr("transform", (d) => `translate(${d.x},${d.y})`)
+      .attr("fill-opacity", 1)
+      .attr("stroke-opacity", 1);
+
+    // Transition exiting nodes to the parent's new position.
+    const nodeExit = node
+      .exit()
+      .transition(transition)
+      .remove()
+      .attr("transform", (d) => `translate(${source.x},${source.y})`)
+      .attr("fill-opacity", 0)
+      .attr("stroke-opacity", 0);
+
+    // Update the links…
+    const link = gLink.selectAll("path").data(links, (d) => d.target.id);
+
+    // Enter any new links at the parent's previous position.
+    const linkEnter = link
+      .enter()
+      .append("path")
+      .attr("d", (d) => {
+        const o = { x: source.x0, y: source.y0 };
+        return diagonal({ source: o, target: o });
+      });
+
+    // Transition links to their new position.
+    link.merge(linkEnter).transition(transition).attr("d", diagonal);
+
+    // Transition exiting nodes to the parent's new position.
+    link
+      .exit()
+      .transition(transition)
+      .remove()
+      .attr("d", (d) => {
+        const o = { x: source.x, y: source.y };
+        return diagonal({ source: o, target: o });
+      });
+
+    // Stash the old positions for transition.
+    root.eachBefore((d) => {
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });
+  }
+
+  update(root);
 }
 
+drawCollapsibleTree();
+
 var slider = document.getElementById("vectorSize");
-slider.addEventListener("change", function() {
-    console.log(slider.value);
-    redrawTree(slider.value);
+slider.addEventListener("change", function () {
+  // TODO: remove this hack
+  d3.select("svg").remove();
+
+  // drawTree(slider.value);
+  drawCollapsibleTree();
+});
+
+var splitter = document.getElementById("vectorSplitter");
+splitter.addEventListener("click", () => {
+  wasm.split_vec(0, 29);
+
+  // TODO: remove this hack
+  d3.select("svg").remove();
+  drawCollapsibleTree();
 });
