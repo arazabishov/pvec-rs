0b4323a0aafb9c89d5a9ac42ebda44c67838772a
Araz Abishov <araz@abishov.com>
2023-07-14 00:00:08 +0200

    feat(web-vis): new navigation design (#99)

--- web-vis/www/src/app.js
+++ web-vis/www/src/app.js
@@ -21,6 +21,8 @@ const trees = [];
 //   // rrbVecTwo.set(vecTwo);
 // });
 
+const grid = addGrid();
+
 function addTree() {
   const vectorIndex = wasm.len();
 
@@ -28,58 +30,121 @@ function addTree() {
   // to the rest of the code below
   wasm.push_vec();
 
-  const treeId = `tree${trees.length}`;
+  const tree = document.createElement("div");
+  tree.id = `tree${trees.length}`;
+  tree.classList.add(
+    "bg-white",
+    "rounded-lg",
+    "border",
+    "border-gray-300",
+    "p-4",
+    "min-h-[600px]",
+    "relative"
+  );
 
-  const div = document.createElement("div");
-  div.id = treeId;
+  const sliderContainer = document.createElement("div");
+  sliderContainer.classList.add("absolute", "w-1/4", "bottom-4", "right-12");
 
-  const sizeSlider = document.createElement("input");
-  sizeSlider.type = "range";
-  sizeSlider.min = 1;
-  sizeSlider.max = 512;
-  sizeSlider.value = 50;
-  sizeSlider.addEventListener("change", () => {
-    wasm.set_vec_size(vectorIndex, sizeSlider.value);
+  const sliderTooltip = document.createElement("output");
+  sliderTooltip.classList.add("tooltip-value");
+
+  const slider = document.createElement("input");
+  slider.type = "range";
+  slider.min = 1;
+  slider.max = 512;
+  slider.value = 50;
+  slider.addEventListener("change", () => {
+    wasm.set_vec_size(vectorIndex, slider.value);
 
     const rrbVecVis = trees[vectorIndex];
     const rrbVec = JSON.parse(wasm.get(vectorIndex));
 
     console.log(rrbVec);
-
     rrbVecVis.set(rrbVec);
   });
 
-  div.appendChild(sizeSlider);
+  sliderContainer.appendChild(slider);
+  sliderContainer.appendChild(sliderTooltip);
 
-  document.body.appendChild(div);
+  const updateTooltip = () => {
+    const offset =
+      ((slider.value - slider.min) * 100) / (slider.max - slider.min);
+    sliderTooltip.innerHTML = `<span>${slider.value}</span>`;
 
-  const rrbVec = new RrbVec(`#${treeId}`);
+    // Kind of magic numbers based on size of the native UI thumb
+    sliderTooltip.style.left = `calc(${offset}% + (${5 - offset * 0.1}px))`;
+  };
+
+  slider.addEventListener("input", updateTooltip);
+  updateTooltip();
+
+  tree.appendChild(sliderContainer);
+  grid.insertBefore(tree, newTreeButton);
+
+  const rrbVec = new RrbVec(`#${tree.id}`);
   trees.push(rrbVec);
 }
 
-function addButton() {
+function addNewTreeButton() {
   const button = document.createElement("button");
-  button.innerHTML = "Add Tree";
   button.type = "button";
   button.classList.add(
+    "bg-white",
+    "rounded-lg",
+    "border",
+    "border-gray-300",
+    "p-4",
+    "min-h-[196px]",
+    "min-w-[196px]",
     "focus:outline-none",
-    "text-blue-600",
-    "text-sm",
     "py-2.5",
     "px-5",
-    "rounded-md",
     "border",
-    "border-blue-600",
-    "hover:bg-blue-50",
-    "fixed",
-    "bottom-4",
-    "right-4"
+    "hover:bg-gray-50",
+    "flex",
+    "items-center",
+    "justify-center"
   );
+
+  const plusIcon = document.createElement("span");
+  plusIcon.classList.add("text-gray-500", "text-4xl");
+  plusIcon.innerHTML = "+";
+
+  button.appendChild(plusIcon);
   button.addEventListener("click", addTree);
 
-  document.body.appendChild(button);
+  const container = document.createElement("div");
+  container.classList.add(
+    "flex",
+    "justify-center",
+    "items-center",
+    "min-h-[600px]"
+  );
+  container.appendChild(button);
+
+  grid.appendChild(container);
+
+  return container;
 }
 
-addButton();
+function addGrid() {
+  const grid = document.createElement("div");
+  grid.classList.add(
+    "grid",
+    "grid-cols-1",
+    "md:grid-cols-1",
+    "lg:grid-cols-2",
+    "gap-4",
+    "m-4"
+  );
+  document.body.appendChild(grid);
+  return grid;
+}
+
+const body = document.body;
+body.style.background =
+  "radial-gradient(#000 1px, transparent 0px) 0 0 / 24px 24px";
+
+const newTreeButton = addNewTreeButton();
 
 addTree();
