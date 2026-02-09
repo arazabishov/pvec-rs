0a8e38616699d40d685bb95e3c36f6dc6ae864cd
Araz Abishov <araz@abishov.com>
2026-02-09 23:56:33 +0100

    feat(web-vis): turn on treeshaking and prod optimizations

--- web-vis/www/webpack.config.js
+++ web-vis/www/webpack.config.js
@@ -2,27 +2,36 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const path = require("path");
 
-module.exports = {
-  entry: "./index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  mode: "development",
-  experiments: {
-    asyncWebAssembly: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
+module.exports = (_, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    entry: "./index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: isProduction ? "[name].[contenthash:8].js" : "[name].bundle.js",
+      chunkFilename: isProduction
+        ? "[id].[contenthash:8].js"
+        : "[id].bundle.js",
+      clean: true,
+    },
+    mode: argv.mode || "development",
+    devtool: isProduction ? "source-map" : "eval-source-map",
+    experiments: {
+      asyncWebAssembly: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "RRBVec visualization",
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "RRBVec visualization",
-    }),
-  ],
+  };
 };
